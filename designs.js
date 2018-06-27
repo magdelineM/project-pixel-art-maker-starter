35818f0c4a3b3cd7eb7265d0a7b6f49c5e99dfd6
magdelineM <40407512+magdelineM@users.noreply.github.com>
2018-06-27 22:23:13 +0200

    Update designs.js

--- designs.js
+++ designs.js
@@ -6,5 +6,40 @@
 function makeGrid() {
 
 // Your code goes here!
-
+var width, height, color;
+var tr ="<tr></tr>"{
+​
+$('#sizePicker').submit(function (event){
+ event.preventDefault();
+ height =$('#inputHeight').val();
+ width =$('#inputWeight').val();
+ makeGrid(height,width);
+​
+});
+function makeGrid(y,z) {
+​
+$('tr').remove();
+  for (var i = 1; i <= y; i++) {
+​
+      $('#pixelCanvas').append($tr);
+​
+      for (var j = 1; j <=z; j++) {
+​
+          $tr.append($('<td></td>'));
+      }
+  }
+​
+​
+}
+$('td').click(function addColor(){
+color = $('#colorPicker').val();
+​
+if ($(this).attr('style')) {
+$(this).removeAttr('style')
+} else{
+$(this).attr('style','background-color:' + color);
+} 
+​
+});
+​
 }
